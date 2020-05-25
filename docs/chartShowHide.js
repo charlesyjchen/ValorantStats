da5a73077f96ec979e881cd3a18891d0150155ee
charlesyjchen <charlesyjchen1@gmail.com>
2020-05-25 15:55:37 -0400

    Poor-mans chart maker

---
+++ docs/chartShowHide.js
@@ -0,0 +1,40 @@
+$( document ).ready(function() {
+	$( ".combatScorePerUsername" ).hide();
+	$( ".winRatioPerUsername" ).hide();
+	$( "#horizontalAxis" ).change(function() {
+		var selectedHorAxis = $( "#horizontalAxis" ).val();
+		var selectedVerAxis = $( "#verticalAxis" ).val();
+		if (selectedHorAxis != "" && selectedVerAxis !=""){
+			if (selectedVerAxis == "combatScorePicker"){
+				$( ".combatScorePerUsername" ).show();
+				$( ".winRatioPerUsername" ).hide();
+			}
+			else{
+				$( ".winRatioPerUsername" ).show();
+				$( ".combatScorePerUsername" ).hide();
+			}
+		}
+		else{
+			$( ".combatScorePerUsername" ).hide();
+			$( ".winRatioPerUsername" ).hide();
+		}
+	});
+	$( "#verticalAxis" ).change(function() {
+		var selectedHorAxis = $( "#horizontalAxis" ).val();
+		var selectedVerAxis = $( "#verticalAxis" ).val();
+		if (selectedHorAxis != "" && selectedVerAxis !=""){
+			if (selectedVerAxis == "combatScorePicker"){
+				$( ".combatScorePerUsername" ).show();
+				$( ".winRatioPerUsername" ).hide();
+			}
+			else{
+				$( ".winRatioPerUsername" ).show();
+				$( ".combatScorePerUsername" ).hide();
+			}
+		}
+		else{
+			$( ".combatScorePerUsername" ).hide();
+			$( ".winRatioPerUsername" ).hide();
+		}
+	});
+});
